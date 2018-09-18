bfd74472594465f324bb260ca842f18046fb15d1
Christopher <c.francis7@live.ca>
2018-09-17 20:28:47 -0400

    Sept 17 2018

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,10 +20,48 @@ class PagesController < ApplicationController
 
 	end	
 	
+	# this is all for the Bookstore
+	def index 
+
+		@title = "Dog Eared Books"
+
+	end
+
+	def category
+
+		@title = "Dog Eared Books | Category"
+
+	end
+
+	def search
+
+		@title = "Dog Eared Books | Search"
+
+	end
+
+	def searchRes
+
+		@title = "Dog Eared Books | Result"
+
+	end
+
+	def cart
+
+		@title = "Dog Eared Books | User's Cart"
+
+	end
+
+	def checkout
+
+		@title = "Dog Eared Books | Checkout"
+
+	end
+
+	def product
+
+		@title = "Dog Eared Books | PROD" #how do I make this dynamic
+
+	end
+
 
 end
-# statc java on class level
-# activerecord
-# create another migration like book (Product). carefull on naming plural vs singular
-# check into git add caseyli
-# 
