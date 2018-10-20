400a425ab4162a10894bb79aaae96b1646f54d8a
Christopher <c.francis7@live.ca>
2018-10-20 19:22:02 -0400

    allow_blank switched from false to true in toy.rb

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,6 +1,6 @@
 class Toy < ApplicationRecord
 
 	validates :name, presence: true
-	validates_length_of :serial_number, minimum: 1, maximum: 20, allow_blank: false
+	validates_length_of :serial_number, minimum: 1, maximum: 20, allow_blank: true
 
 end
