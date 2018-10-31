de653e00df1fbe621471a0f99d5607c6a159789a
Christopher <c.francis7@live.ca>
2018-10-31 17:54:23 -0400

    fixed stashed cache with updated info

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,6 +23,19 @@ class BooksController < ApplicationController
 	def show
 		@title = "Books Show"
 		@book = Book.find(params[:id])
+
+		# random number list to populate 5 'related' books under product
+		# array is populated with 5 unique random numbers used to find id from DB
+		@randomNumArray = [] 
+		i = 0 # counter to determine when 5 numbers have been added to array
+		largestId = Book.last.id
+		while i < 5 do  
+			randNum = rand(1..largestId)
+			if(!@randomNumArray.include? randNum) # ensures that the number isnt in the array
+				@randomNumArray[i] = randNum
+				i += 1;
+			end
+		end
 	end
 
 	def edit
