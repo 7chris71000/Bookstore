852815c2400928a48d1372edc4b9ddb528349cef
Christopher <c.francis7@live.ca>
2018-11-02 17:56:36 -0400

    Changed variable names to fit ruby conventions. Changed title of show page

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -19,18 +19,20 @@ class ToysController < ApplicationController
 	end
 
 	def show
-		@title = "Edit Toy"
+		
 		@toy = Toy.find(params[:id])
+
+		@title = "#{@toy.name}"
 
 		# random number list to populate 5 'related' toys under product
 		# array is populated with 5 unique random numbers used to find id from DB
-		@randomNumArray = [] 
+		@random_number_array = [] 
 		i = 0 # counter to determine when 5 numbers have been added to array
-		largestId = Toy.last.id
+		largest_id = Toy.last.id
 		while i < 5 do  
-			randNum = rand(1..largestId)
-			if(!@randomNumArray.include? randNum) # ensures that the number isnt in the array
-				@randomNumArray[i] = randNum
+			random_number = rand(1..largest_id)
+			if(!@random_number_array.include? random_number) # ensures that the number isnt in the array
+				@random_number_array[i] = random_number
 				i += 1;
 			end
 		end
