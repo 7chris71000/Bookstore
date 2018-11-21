33160f17a9d6d055704bff90f69aaae20e114fcf
Christopher <c.francis7@live.ca>
2018-11-21 18:02:24 -0500

    added scaffold blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_220343) do
+ActiveRecord::Schema.define(version: 2018_11_21_225800) do
+
+  create_table "blog_posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_10_08_220343) do
     t.datetime "updated_at", null: false
     t.string "image_url"
     t.integer "published_year"
+    t.decimal "price"
   end
 
   create_table "toys", force: :cascade do |t|
