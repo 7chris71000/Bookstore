62eb22521045e022ede6d0d0540382dca92a526f
Christopher <c.francis7@live.ca>
2018-11-24 18:36:07 -0500

    Formatted CSS for blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_225800) do
+ActiveRecord::Schema.define(version: 2018_11_24_021217) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,37 @@ ActiveRecord::Schema.define(version: 2018_11_21_225800) do
     t.string "image_url"
     t.integer "published_year"
     t.decimal "price"
+  end
+
+  create_table "post_tags", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "author"
+    t.integer "star_rating"
+    t.text "comment"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "toy_reviews", force: :cascade do |t|
+    t.string "author"
+    t.integer "star_rating"
+    t.text "comment"
+    t.integer "toy_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "toys", force: :cascade do |t|
