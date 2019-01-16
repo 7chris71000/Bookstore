6b72c15b3d5b5ac74b77fa966441884e49f4f73a
Christopher <c.francis7@live.ca>
2019-01-16 18:31:16 -0500

    Started to flesh out users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,23 +2,56 @@ class UsersController < ApplicationController
 
 	before_action :deny_access_for_non_signed_in_users
 
-	# def index
+	def index
 		
-	# 	@user = User.new
-	# 	@users = User.all
+		@users = User.all
 
-	# 	@title = "Users"
-
-	# end
-
-	def show
-	
-		@title = User.find(params[:id]).name
-
-		@profile = User.find(params[:id])
-
+		@title = "Users"
 
 	end
+
+	def show
+
+		@user = User.find(params[:id])
+	
+		@title = @user.name
+
+	end
+
+	def edit
+		@user = User.find(params[:id])
+
+		if current_user != @user
+			redirect_to new_user_session_path
+		end
+
+	end
+
+	def new
+		@user =User.new
+
+		if !current_user.admin
+			redirect_to root_path
+		end
+
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to users_path
+		else
+			render 'new'
+		end
+		
+	end 
+
+	def update
+	end
+
+	def destroy 
+	end
+
 
 
 	# def create
@@ -34,6 +67,11 @@ class UsersController < ApplicationController
 	# end
 
 	private
+
+		def user_params
+			params.require(:user).permit(:email, :name, :age, :password, :password_confirmation)
+		
+		end 
 
 		def deny_access_for_non_signed_in_users
 			if !user_signed_in?
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :users
   devise_for :users
-  resources :users, only: [:show] #This is to find the users profile
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#home"
 
